cfc9a00a6119bc6c4a5042861d597bb897a755ab
TedOnyango <teddonyango@gmail.com>
2019-08-30 19:39:34 +0300

    Initial commit

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,16 @@
+package emailapp;
+
+public class EmailApp {
+
+
+
+    public static void main(String[] args) {
+        emailapp.Email email1 = new emailapp.Email("John", "Smith");
+
+        System.out.println(email1.showInfo());
+
+    }
+
+
+
+}
